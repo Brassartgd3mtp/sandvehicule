ef58c38a7a6ea573036c76a6986be434ff480500
Alexis DARRAS <alexis.darras@brassart.fr>
2023-10-09 16:48:59 +0200

    Ajout Enchainement Explo / Tempête

--- Assets/GameSystem.cs
+++ Assets/GameSystem.cs
@@ -1,20 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class GameSystem : MonoBehaviour
 {
+    public WaveSystem waveSystem;
+
     public bool stormIsActive;
 
+    public float timerOfStorm;
+
+    public Slider timerSlider;
+    public float timeBeforeStorm;
+
+    private bool stopTimer;
 
     void Start()
     {
+        waveSystem = GetComponent<WaveSystem>();
+
         stormIsActive = false;
+        stopTimer = false;
+
+        //timeBeforeStorm = 10;
+        //timerOfStorm = 20;
+
+        timerSlider.maxValue = timeBeforeStorm;
+        timerSlider.value = timeBeforeStorm;
     }
     void Update()
     {
-        
+        if (stormIsActive == false) // Si il n'y a pas de tempête le timer avant activation de la tempête continu
+        {
+            timeBeforeStorm -= Time.deltaTime;
+        }
+
+        if (stopTimer == false) // ce qui relie le slider au temps avant la tempête 
+        {
+            timerSlider.value = timeBeforeStorm;
+        }
+
+        if (timeBeforeStorm <= 0) // la tempête s'active
+        {
+            ActiveWave();
+        }
+
+        if (stormIsActive == true) // le Timer de la tempête débute
+        {
+            timerOfStorm -= Time.deltaTime;
+        }
+        if (timerOfStorm < 0)
+        {
+            EndWave();
+        }
     }
 
+    public void ActiveWave() // lance la vague d'ennemi à la fin du timer de "timeBeforeThe Storm"
+    {
+        stormIsActive = true;
+        waveSystem.StartCoroutine("EnemyDrop");
+        timeBeforeStorm = 10;
+        stopTimer = true;
+    }
 
+    public void EndWave() // Fin de la wave, reset les valeurs pour repartir en exploration
+    {
+        stormIsActive = false;
+        timerOfStorm = 20;
+        stopTimer = false;
+    }
 }
--- Assets/WaveSystem.cs
+++ Assets/WaveSystem.cs
@@ -6,9 +6,11 @@ using UnityEngine.InputSystem;
 public class WaveSystem : MonoBehaviour
 {
     public GameObject enemy;
-    public int xPos;
-    public int zPos;
-
+    
+    public int xPos1, xPos2;
+    public int zPos1, zPos2;
+    public int randomX, randomZ;
+    public float valueForRandomX, valueForRandomZ;
     public GameSystem gameSystem;
     public GameObject player;
 
@@ -29,21 +31,30 @@ public class WaveSystem : MonoBehaviour
 
     }
 
-    public void ActiveWave(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            StartCoroutine(EnemyDrop());
-        }
-    }
 
-    IEnumerator EnemyDrop()
+
+    public IEnumerator EnemyDrop() // 
     {
         while (gameSystem.stormIsActive == true)
         {
-            xPos = Random.Range(-20, 20);
-            zPos = Random.Range(-20, 20);
-            Instantiate (enemy, new Vector3((player.transform.position.x + xPos),player.transform.position.y, player.transform.position.z + zPos), Quaternion.identity);
+            // Récupère des random pour X et Z pour le swpan du prochain ennemi, et cela toutes les 3 secondes
+            xPos1 = Random.Range(15, 25);
+            xPos2 = Random.Range(-25, -15);
+            valueForRandomX = Random.Range(0f, 1f);
+            if (valueForRandomX >= 0.5)
+            {
+                randomX = xPos1;
+            } else if (valueForRandomX < 0.5) { randomX = xPos2; }
+
+            zPos1 = Random.Range(15, 25);
+            zPos2 = Random.Range(-25, -15);
+            valueForRandomZ = Random.Range(0f,1f);
+            if (valueForRandomZ >= 0.5)
+            {
+                randomZ = zPos1;
+            } else if (valueForRandomZ < 0.5) { randomZ = zPos2; } 
+
+            Instantiate (enemy, new Vector3((player.transform.position.x + randomX),player.transform.position.y, player.transform.position.z + randomZ), Quaternion.identity);
             yield return new WaitForSeconds(3);
         }
     }
