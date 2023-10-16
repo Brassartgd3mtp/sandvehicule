9c7735dc7c90c646ae51333c431a33c942b018f7
Alexis DARRAS <alexis.darras@brassart.fr>
2023-10-16 11:47:40 +0200

    Ajout CoolDown de tir

--- Assets/200_Scripts/Player/Attack/AttackManager.cs
+++ Assets/200_Scripts/Player/Attack/AttackManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,19 +11,30 @@ public class AttackManager : MonoBehaviour
     public GameObject BaseProjectile;
     public PlayerStates playerStates;
 
+    public bool isShooting;
+    public float attackSpeed;
+    public float cooldownAttack;
+
     public int speed;
 
     void Start()
     {
+        cooldownAttack = 0;
         playerStates = GetComponent<PlayerStates>();
     }
 
     void Update()
     {
-        if (playerStates.states == PlayerStates.States.Fifhting)
+        if (isShooting == true && cooldownAttack <= 0) 
         {
-
+            Instantiate(BaseProjectile, shootingPos.transform.position, shootingPos.transform.rotation);
+            cooldownAttack = 1 / attackSpeed;
         }
+        if (cooldownAttack > 0)
+        {
+            cooldownAttack -= Time.deltaTime;
+        }
+
     }
 
     public void BasicShoot(InputAction.CallbackContext context) //Méthode qui permet de tirer
@@ -30,13 +42,36 @@ public class AttackManager : MonoBehaviour
         switch (context.phase)
         {
             case InputActionPhase.Performed:
+
                 if (playerStates.states == PlayerStates.States.Fifhting) // permet de tirer uniquement pendant le mode de combat 
                 {
-                    Instantiate(BaseProjectile, shootingPos.transform.position, shootingPos.transform.rotation);
+                    isShooting = true;
                 }
                 
                 //projectile.transform.position += transform.forward * Time.deltaTime * speed;
                 break;
+            case InputActionPhase.Canceled:
+                if (playerStates.states == PlayerStates.States.Fifhting)
+                {
+                    isShooting = false;
+                }
+                break;
+
         }
     }
+
+    public IEnumerator ShootInContinue()
+    {
+        do
+        {
+          
+          if (isShooting == false)
+            {
+               StopCoroutine(ShootInContinue());
+            }
+          yield return new WaitForSeconds(1/ attackSpeed);
+        }
+        while (isShooting == true) ;
+    }
+
 }
