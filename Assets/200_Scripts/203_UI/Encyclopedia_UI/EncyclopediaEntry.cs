24fa6ec78c5968b5872bd1d784bc18d3519b108e
Nicolas IMBERT <nicolas.imbert@brassart.fr>
2023-11-29 17:38:14 +0100

    Debut script Encyclopédie

---
+++ Assets/200_Scripts/203_UI/Encyclopedia_UI/EncyclopediaEntry.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncyclopediaEntry : MonoBehaviour
+{
+    public int associatedEntryID;
+    public EncyclopediaManager encyclopediaManager;
+    public LayerMask playerLayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Vérifie si le joueur est dans le layer spécifié par le LayerMask
+        if (((1 << other.gameObject.layer) & playerLayer) != 0)
+        {
+            encyclopediaManager.AddEntry(associatedEntryID);
+            // Ajoute d'autres actions si nécessaire (comme désactiver l'objet pour éviter une double entrée)
+            Destroy(gameObject);
+        }
+    }
+}
