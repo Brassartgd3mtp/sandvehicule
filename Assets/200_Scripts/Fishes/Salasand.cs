dca1a7acd88ebd3af7e46f6fe2c1e5bafedf941f
Florian BAUMANN <florian.baumann@brassart.fr>
2023-12-18 10:41:08 +0100

    Salasand SoundKey Anim - Salasand Sounds Script

--- Assets/200_Scripts/Fishes/Salasand.cs
+++ Assets/200_Scripts/Fishes/Salasand.cs
@@ -11,4 +11,24 @@ public class Salasand : FishBehavior
     {
         isReadyToBeCaught = true;
     }
+
+    public void PlaySalasandJump()
+    {
+        AudioSource audioSource = GetComponent <AudioSource>();
+        AudioManager.Instance.PlaySound( 4, audioSource);
+    }
+    public void PlaySalasandMoves()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        int[] soundsIdsSand = { 6, 7, 8 };
+        int randomIndexSand = Random.Range(0, soundsIdsSand.Length);
+        int randomSoundIdSand = soundsIdsSand[randomIndexSand];
+        AudioManager.Instance.PlaySound( 3, audioSource);
+    }
+
+public void PlaySalasandDive()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        AudioManager.Instance.PlaySound(3, audioSource);
+    }
 }
