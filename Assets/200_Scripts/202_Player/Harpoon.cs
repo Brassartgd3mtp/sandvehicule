0b036456ab6a68b993e4f63e2ba44b9f0de0bb48
Alexis DARRAS <alexis.darras@brassart.fr>
2023-12-19 15:36:10 +0100

    Debug build

--- Assets/200_Scripts/202_Player/Harpoon.cs
+++ Assets/200_Scripts/202_Player/Harpoon.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Cinemachine;
-using TreeEditor;
 using UnityEngine.Rendering;
 using UnityEngine.EventSystems;
 using Unity.VisualScripting;
