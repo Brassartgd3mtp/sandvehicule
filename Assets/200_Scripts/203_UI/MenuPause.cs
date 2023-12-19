bdd41f76ba78e834aff62b52419ea8bf449f8209
PapiTac <imbert.nicolas0@gmail.com>
2023-12-19 19:28:38 +0100

    Menu Pause

---
+++ Assets/200_Scripts/203_UI/MenuPause.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    private bool isPaused = false;
+    public GameObject PauseDisplay;
+    public GameObject InputDisplay;
+    private bool InputActive = false;
+    public string SceneToLoad;
+
+    private void Start()
+    {
+        PauseDisplay.SetActive(false);
+        ResumeGame();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                if (InputActive)
+                {
+                    InputDisplay.SetActive(false);
+                    InputActive = false;
+                }
+                else
+                {
+                    ResumeGame();
+                }
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        PauseDisplay.gameObject.SetActive(true);
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        PauseDisplay.gameObject.SetActive(false);
+    }
+
+    public void ResumeButton()
+    {
+        Time.timeScale = 1f;
+        PauseDisplay.gameObject.SetActive(false);
+    }
+    public void InputButton()
+    {
+        InputDisplay.gameObject.SetActive(true);
+        InputActive = true;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+    public void QuitGameButton()
+    {
+        Application.Quit();
+    }
+}
